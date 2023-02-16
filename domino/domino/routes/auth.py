8f4ed87f06ec134cebe9d80c7acb5a1c37c9d5d9
migue <miguel.lau@etecsa.cu>
2023-02-16 10:11:46 -0500

    Modificando la creación de usuario

--- domino/domino/routes/auth.py
+++ domino/domino/routes/auth.py
@@ -2,7 +2,7 @@
 
 from fastapi import APIRouter, Depends, HTTPException, Request
 from domino.schemas.user import UserLogin
-from domino.services.auth import auth 
+from domino.services.auth import auth
 
 from sqlalchemy.orm import Session
 from domino.app import get_db
@@ -20,15 +20,18 @@ from domino.app import _
 
 auth_routes = APIRouter()
 
+
 @auth_routes.post("/login", status_code=status.HTTP_200_OK, tags=["Autentificación"], summary="Autentificación en la API")
-def login(request:Request, user: UserLogin, db: Session = Depends(get_db)):
+def login(request: Request, user: UserLogin, db: Session = Depends(get_db)):
     return auth(request=request, db=db, user=user)
 
+
 @auth_routes.get('/me', summary="Obtiene información del usuario autentificado", tags=["Autentificación"], dependencies=[Depends(JWTBearer())])
-async def get_me(request:Request):
-    user = get_current_user(request)       
+async def get_me(request: Request):
+    user = get_current_user(request)
     return JSONResponse(content=user, status_code=200)
 
-@auth_routes.post("/users", response_model=ResultObject, tags=["Autentificación"], summary="Crear un Usuario")
-def create_user(request:Request, user: UserCreate, db: Session = Depends(get_db)):    
+
+@auth_routes.post("/register", response_model=ResultObject, tags=["Autentificación"], summary="Registrar un usuario en la plataforma")
+def create_user(request: Request, user: UserCreate, db: Session = Depends(get_db)):
     return new(request=request, user=user, db=db)
