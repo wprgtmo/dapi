ee076bacac8a9c790f16aad299297b9988ad9e00
migue <miguel.lau@etecsa.cu>
2023-05-02 09:43:08 -0400

    prueba de subir imagenes

--- domino/domino/app.py
+++ domino/domino/app.py
@@ -1,8 +1,8 @@
 # app.py
 
-from fastapi import FastAPI, Request, UploadFile, File, Header, Response
+from fastapi import FastAPI, HTTPException, Request, UploadFile, File, Header, Response, status
 from fastapi.responses import FileResponse, JSONResponse, StreamingResponse
-from os import getcwd, remove, path
+from os import getcwd, remove, path, stat
 
 from pyi18n import PyI18n
 from domino.config.config import settings
@@ -12,7 +12,7 @@ from logging.config import dictConfig
 from domino.config.db import SessionLocal
 from fastapi.openapi.docs import (get_redoc_html, get_swagger_ui_html, get_swagger_ui_oauth2_redirect_html)
 from fastapi.staticfiles import StaticFiles
-from typing import Callable
+from typing import BinaryIO, Callable
 import shutil
 
 
@@ -115,12 +115,12 @@ def getEventImage(user_id: str, event_id: str, file_name: str):
 def getPostFile(post_id: str, file_name: str):
     return FileResponse(getcwd() + "/public/post/" + post_id + "/" + file_name)
 
-@app.get("/{video_name}")
-def get_video(video_name: str):
-    file_name = getcwd()+"/public/post/316b0eeb-237a-4903-94d7-5686772468d5/"+video_name
-    with open(file_name, "rb") as myfile:
-        data = myfile.read(path.getsize(file_name))
-        return Response(content=data, status_code=206, media_type="video/mp4")
+# @app.get("/{video_name}")
+# def get_video(video_name: str):
+#     file_name = getcwd()+"/public/post/316b0eeb-237a-4903-94d7-5686772468d5/"+video_name
+#     with open(file_name, "rb") as myfile:
+#         data = myfile.read(path.getsize(file_name))
+#         return Response(content=data, status_code=206, media_type="video/mp4")
 
 
 @app.delete("/delete/{name}")
@@ -134,3 +134,76 @@ def delevent(name: str):
 @app.get("/{user_id}/{name}")
 def getprofile(user_id: str, file_name: str):
     return FileResponse(getcwd() + "/public/profile/" + user_id + "/" + file_name)
+
+def send_bytes_range_requests(
+    file_obj: BinaryIO, start: int, end: int, chunk_size: int = 10_000
+):
+    """Send a file in chunks using Range Requests specification RFC7233
+
+    `start` and `end` parameters are inclusive due to specification
+    """
+    with file_obj as f:
+        f.seek(start)
+        while (pos := f.tell()) <= end:
+            read_size = min(chunk_size, end + 1 - pos)
+            yield f.read(read_size)
+            
+def _get_range_header(range_header: str, file_size: int) -> tuple[int, int]:
+    def _invalid_range():
+        return HTTPException(
+            status.HTTP_416_REQUESTED_RANGE_NOT_SATISFIABLE,
+            detail=f"Invalid request range (Range:{range_header!r})",
+        )
+
+    try:
+        h = range_header.replace("bytes=", "").split("-")
+        start = int(h[0]) if h[0] != "" else 0
+        end = int(h[1]) if h[1] != "" else file_size - 1
+    except ValueError:
+        raise _invalid_range()
+
+    if start > end or start < 0 or end > file_size - 1:
+        raise _invalid_range()
+    return start, end
+
+def range_requests_response(
+    request: Request, file_path: str, content_type: str
+):
+    """Returns StreamingResponse using Range Requests of a given file"""
+
+    file_size = stat(file_path).st_size
+    range_header = request.headers.get("range")
+
+    headers = {
+        "content-type": content_type,
+        "accept-ranges": "bytes",
+        "content-encoding": "identity",
+        "content-length": str(file_size),
+        "access-control-expose-headers": (
+            "content-type, accept-ranges, content-length, "
+            "content-range, content-encoding"
+        ),
+    }
+    start = 0
+    end = file_size - 1
+    status_code = status.HTTP_200_OK
+
+    if range_header is not None:
+        start, end = _get_range_header(range_header, file_size)
+        size = end - start + 1
+        headers["content-length"] = str(size)
+        headers["content-range"] = f"bytes {start}-{end}/{file_size}"
+        status_code = status.HTTP_206_PARTIAL_CONTENT
+
+    return StreamingResponse(
+        send_bytes_range_requests(open(file_path, mode="rb"), start, end),
+        headers=headers,
+        status_code=status_code,
+    )
+
+@app.get("/{post_id}/{video_name}")
+def get_video(request: Request, post_id: str, video_name: str):
+    file_name = getcwd() + "/public/post/" + post_id + "/" + video_name
+    return range_requests_response(
+        request, file_path=file_name, content_type="video/mp4"
+    )
