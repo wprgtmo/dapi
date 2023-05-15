d485043c189f767641b498f77c479995c18c98c2
migue <miraidys.garcia@etecsa.cu>
2023-05-15 17:50:57 -0400

    ejecitar en bd

--- backup/ejecutar_base_dato.sql
+++ backup/ejecutar_base_dato.sql
@@ -1,2 +1,9 @@
 ALTER TABLE IF EXISTS enterprise.users
     ADD COLUMN receive_notifications boolean DEFAULT False;
+
+INSERT INTO resources.event_roles(name, description, created_by, created_date)
+		VALUES ('PLAYER', 'JUGADOR', 'miry', '2023-05-12 00:00:00');
+	INSERT INTO resources.event_roles(name, description, created_by, created_date)
+		VALUES ('REFEREE', 'ARBITRO', 'miry', '2023-05-12 00:00:00');
+	INSERT INTO resources.event_roles(name, description, created_by, created_date)
+		VALUES ('JOURNALIST', 'PERIODISTA', 'miry', '2023-05-12 00:00:00');
