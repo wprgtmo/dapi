0c5afcb3ffe80aa81489b64ef6a724ed010ad565
migue <miraidys.garcia@etecsa.cu>
2023-05-15 18:10:08 -0400

    creando tabla de roles event

--- backup/ejecutar_base_dato.sql
+++ backup/ejecutar_base_dato.sql
@@ -1,28 +1,3 @@
 ALTER TABLE IF EXISTS enterprise.users
     ADD COLUMN receive_notifications boolean DEFAULT False;
 
-DROP TABLE IF EXISTS resources.event_roles;
-
-CREATE TABLE IF NOT EXISTS resources.event_roles
-(
-    id integer NOT NULL DEFAULT nextval('resources.event_roles_id_seq'::regclass),
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    description character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    created_by character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    created_date timestamp without time zone NOT NULL,
-    CONSTRAINT event_roles_pkey PRIMARY KEY (id),
-    CONSTRAINT event_roles_name_key UNIQUE (name)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS resources.event_roles
-    OWNER to postgres;
-
-    
-INSERT INTO resources.event_roles(name, description, created_by, created_date)
-		VALUES ('PLAYER', 'JUGADOR', 'miry', '2023-05-12 00:00:00');
-	INSERT INTO resources.event_roles(name, description, created_by, created_date)
-		VALUES ('REFEREE', 'ARBITRO', 'miry', '2023-05-12 00:00:00');
-	INSERT INTO resources.event_roles(name, description, created_by, created_date)
-		VALUES ('JOURNALIST', 'PERIODISTA', 'miry', '2023-05-12 00:00:00');
