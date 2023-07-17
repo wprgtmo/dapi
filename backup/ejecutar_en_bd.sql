deac58b9b7fa4a77954fdfc21ec82262c094d94d
migue <miraidys.garcia@etecsa.cu>
2023-07-17 11:50:27 -0400

    oyr

--- backup/ejecutar_en_bd.sql
+++ backup/ejecutar_en_bd.sql
@@ -1,8 +1,5 @@
 ALTER TABLE IF EXISTS events.events
     ADD COLUMN profile_id character varying;
-
-ALTER TABLE IF EXISTS events.events
-    ADD COLUMN profile_id character varying;
 ALTER TABLE IF EXISTS events.events
     ADD CONSTRAINT profile_id_fkey FOREIGN KEY (profile_id)
     REFERENCES enterprise.profile_member (id) MATCH SIMPLE
