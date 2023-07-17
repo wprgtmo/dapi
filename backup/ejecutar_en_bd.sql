5d274b77d3be410122e3b9f8739df7782fed3baa
migue <miraidys.garcia@etecsa.cu>
2023-07-17 11:50:06 -0400

    Paar BASED

--- backup/ejecutar_en_bd.sql
+++ backup/ejecutar_en_bd.sql
@@ -1,2 +1,11 @@
-ALTER TABLE IF EXISTS enterprise.profile_users
-    ADD COLUMN is_confirmed boolean DEFAULT False;
+ALTER TABLE IF EXISTS events.events
+    ADD COLUMN profile_id character varying;
+
+ALTER TABLE IF EXISTS events.events
+    ADD COLUMN profile_id character varying;
+ALTER TABLE IF EXISTS events.events
+    ADD CONSTRAINT profile_id_fkey FOREIGN KEY (profile_id)
+    REFERENCES enterprise.profile_member (id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    NOT VALID;
