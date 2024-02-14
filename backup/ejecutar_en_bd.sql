da8c277053a0e0ba6fceb0f3edde0e1737975e4e
migue <miraidys.garcia@etecsa.cu>
2024-02-14 10:13:56 -0500

    upt

--- backup/ejecutar_en_bd.sql
+++ backup/ejecutar_en_bd.sql
@@ -7,7 +7,3 @@ ADD COLUMN motive_not_valid_description character varying;
 ALTER TABLE IF EXISTS events.domino_boletus_position
 ADD COLUMN is_guilty_closure boolean;
 
----
-ALTER TABLE IF EXISTS events.domino_boletus_position
-ADD COLUMN player_id character varying;
-    
