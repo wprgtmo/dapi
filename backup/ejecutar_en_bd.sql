d43c4933e38227438c3e0881c037c63e73c0554b
migue <miraidys.garcia@etecsa.cu>
2024-03-13 10:02:55 -0400

    update

--- backup/ejecutar_en_bd.sql
+++ backup/ejecutar_en_bd.sql
@@ -1,13 +1,6 @@
 ALTER TABLE IF EXISTS enterprise.profile_event_admon
-ADD COLUMN federation_id integer;
+ADD COLUMN profile_user_id character varying;
 ALTER TABLE IF EXISTS enterprise.profile_federated
-ADD COLUMN federation_id integer;
+ADD COLUMN profile_user_id character varying;
 ALTER TABLE IF EXISTS enterprise.profile_referee
-ADD COLUMN federation_id integer;
-
-ALTER TABLE IF EXISTS enterprise.profile_single_player
-ADD COLUMN club_id integer;
-ALTER TABLE IF EXISTS enterprise.profile_pair_player
-ADD COLUMN club_id integer;
-ALTER TABLE IF EXISTS enterprise.profile_team_player
-ADD COLUMN club_id integer;
+ADD COLUMN profile_user_id character varying;
